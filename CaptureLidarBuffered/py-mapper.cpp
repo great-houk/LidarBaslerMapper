138676f75e51fa51682cdf9dbb29bdbb9120151b
Tyler Richard <73250404+great-houk@users.noreply.github.com>
2023-03-27 13:10:44 -0400

    oopsie

--- CaptureLidarBuffered/py-mapper.cpp
+++ CaptureLidarBuffered/py-mapper.cpp
@@ -8,7 +8,7 @@ PYBIND11_MODULE(pymapper, m) {
     m.def("stop", &LidarCapture::stop);
     m.def("find_sphere", &LidarCapture::findSphere);
     
-    m.def("test", &[const std::string &s] { std::cout << s << std::endl; });
+    m.def("test", [](const std::string &s) { std::cout << s << std::endl; });
 
     py::class_<sphereCenter>(m, "sphereCenter")
             .def_readonly("x", &sphereCenter::x)
@@ -17,5 +17,5 @@ PYBIND11_MODULE(pymapper, m) {
             .def_readonly("r", &sphereCenter::r)
             .def("__repr__", [](const sphereCenter& s){
                 return "sphereCenter {\n\tx: " + std::to_string(s.x) + "\n\ty: " + std::to_string(s.y) + "\n\tz: " + std::to_string(s.z) + "\n\tr: " + std::to_string(s.r) + "\n}";
-            });;
+            });
 }
